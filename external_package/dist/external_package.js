404d9d4ef7f928f20e287d5366815400986b915b
Matthew Soulanille <msoulanille@google.com>
2021-01-19 10:00:42 -0800

    Ignore dist

--- external_package/dist/external_package.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bar_1 = require("@tests/bar/bar");
-console.log(bar_1.bar());
-//# sourceMappingURL=external_package.js.map
